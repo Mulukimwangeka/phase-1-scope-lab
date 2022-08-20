b556579b63e39ea602043c3ef1476c4855e00f29
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-20 17:34:50 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName ='bob';
+const leastFavouriteCustomer ='you';
+function  upperCaseCustomerName(){
+   return customerName= customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer ='not bob'
+} 
+function overwriteBestCustomer(newfav){
+    bestCustomer = newfav;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer='me'
+}
